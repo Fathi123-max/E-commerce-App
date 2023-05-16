7c798ca327d588ca054ff1a689fe9f6062eb7278
fathi mohamed fathi wehba <allahakbar@fathis-Mac-Pro.local>
2023-05-15 23:20:24 -0700

    1 day 10

--- lib/view/pages/product_view/new_product_view.dart
+++ lib/view/pages/product_view/new_product_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 /// Flutter code sample for [SliverAppBar].
 
@@ -9,10 +10,23 @@ class AppBarApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(useMaterial3: true),
-      debugShowCheckedModeBanner: false,
-      home: const SliverAppBarExample(),
+    return ScreenUtilInit(
+      designSize: const Size(360, 690),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (context, child) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'First Method',
+          // You can use the library anywhere in the app even in theme
+          theme: ThemeData(
+            useMaterial3: true,
+            primarySwatch: Colors.green,
+          ),
+          home: child,
+        );
+      },
+      child: const SliverAppBarExample(),
     );
   }
 }
@@ -34,62 +48,211 @@ class _SliverAppBarExampleState extends State<SliverAppBarExample> {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    return Scaffold(
-      body: CustomScrollView(
-        slivers: <Widget>[
-          SliverAppBar(
-            pinned: _pinned,
-            snap: _snap,
-            backgroundColor: Colors.amber,
-            floating: _floating,
-            expandedHeight: size.height * .5.h,
-            flexibleSpace: Container(
-                child: Stack(
-              children: [
+    return SafeArea(
+      child: Scaffold(
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: const Icon(Icons.add),
+        ),
+        body: CustomScrollView(
+          slivers: <Widget>[
+            SliverAppBar(
+              pinned: _pinned,
+              snap: _snap,
+              backgroundColor: Colors.amber,
+              floating: _floating,
+              expandedHeight: size.height * 1.2,
+              flexibleSpace: Stack(children: [
+                const Positioned.fill(
+                    top: 0, left: 0, right: 0, child: FlutterLogo()),
                 Positioned(
-                  bottom: 0,
-                  right: 0,
-                  child: ElevatedButton(
-                      onPressed: () {}, child: const FlutterLogo()),
-                )
+                    bottom: 10,
+                    right: 30,
+                    child: Container(
+                      height: 40,
+                      width: 40,
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(30)),
+                      child: const Icon(Icons.favorite),
+                    )),
+                Positioned(
+                    bottom: 10,
+                    left: 30,
+                    child: Container(
+                      height: 40,
+                      width: 40,
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(30)),
+                      child: const Icon(Icons.arrow_back),
+                    )),
+                Positioned(
+                    left: 0,
+                    right: 0,
+                    bottom: 0,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(30.r),
+                          topRight: Radius.circular(30.r),
+                        ),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.withOpacity(0.5),
+                            spreadRadius: 2,
+                            blurRadius: 7,
+                            offset: const Offset(0, 3),
+                          ),
+                        ],
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const SizedBox(height: 10),
+                          ListTile(
+                            title: Text(
+                              "Product Name",
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20.sp,
+                              ),
+                            ),
+                            subtitle: const Wrap(
+                              crossAxisAlignment: WrapCrossAlignment.center,
+                              children: [
+                                Icon(Icons.star, color: Colors.amber, size: 18),
+                                Icon(Icons.star, color: Colors.amber, size: 18),
+                                Icon(Icons.star, color: Colors.amber, size: 18),
+                                Icon(Icons.star_border, size: 18),
+                                Icon(Icons.star_border, size: 18),
+                                SizedBox(width: 5),
+                                Text(
+                                  "4.0 (23 Reviews)",
+                                  style: TextStyle(fontSize: 12),
+                                ),
+                              ],
+                            ),
+                          ),
+                          const Divider(),
+                          const SizedBox(height: 10),
+                          Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 20),
+                              child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    const Text("Product Description"),
+                                    const SizedBox(height: 10),
+                                    Text(
+                                      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis pretium metus non mi bibendum eleifend. Suspendisse vel est quam. Duis pretium metus non mi bibendum eleifend. Suspendisse vel est quam. Duis pretium metus non mi bibendum eleifend. Suspendisse vel est quam. Duis pretium metus non mi bibendum eleifend. Suspendisse vel est quam.",
+                                      textAlign: TextAlign.justify,
+                                      style: TextStyle(
+                                        fontSize: 12.sp,
+                                        height: 1.5,
+                                      ),
+                                    ),
+                                    const SizedBox(height: 10),
+                                    const Text("Product Price"),
+                                    const SizedBox(height: 5),
+                                    Row(
+                                      children: [
+                                        const Text(
+                                          "\$",
+                                          style: TextStyle(
+                                              fontWeight: FontWeight.bold,
+                                              fontSize: 16),
+                                        ),
+                                        Text(
+                                          "59.99",
+                                          style: TextStyle(
+                                            fontWeight: FontWeight.bold,
+                                            fontSize: 30.sp,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                    const SizedBox(height: 10),
+                                    const Text("Product Properties"),
+                                    const SizedBox(height: 10),
+                                    const Row(
+                                      children: [
+                                        Expanded(
+                                          child: Column(
+                                            crossAxisAlignment:
+                                                CrossAxisAlignment.start,
+                                            children: [
+                                              Text(
+                                                "Brand",
+                                                style: TextStyle(
+                                                    fontWeight:
+                                                        FontWeight.bold),
+                                              ),
+                                              SizedBox(height: 5),
+                                              Text("MyBrand"),
+                                            ],
+                                          ),
+                                        ),
+                                        Expanded(
+                                          child: Column(
+                                            crossAxisAlignment:
+                                                CrossAxisAlignment.start,
+                                            children: [
+                                              Text(
+                                                "Condition",
+                                                style: TextStyle(
+                                                    fontWeight:
+                                                        FontWeight.bold),
+                                              ),
+                                              SizedBox(height: 5),
+                                              Text("New"),
+                                            ],
+                                          ),
+                                        ),
+                                        Expanded(
+                                          child: Column(
+                                            crossAxisAlignment:
+                                                CrossAxisAlignment.start,
+                                            children: [
+                                              Text(
+                                                "Quantity",
+                                                style: TextStyle(
+                                                    fontWeight:
+                                                        FontWeight.bold),
+                                              ),
+                                              SizedBox(height: 5),
+                                              Text("1 Available"),
+                                            ],
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                    const SizedBox(height: 20),
+                                  ]))
+                        ],
+                      ),
+                    ))
+              ]),
+            ),
+          ],
+        ),
+        bottomNavigationBar: BottomAppBar(
+          child: Padding(
+            padding: const EdgeInsets.all(8),
+            child: OverflowBar(
+              overflowAlignment: OverflowBarAlignment.center,
+              children: <Widget>[
+                Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    const Text("data"),
+                    const Spacer(),
+                    ElevatedButton(onPressed: () {}, child: const FlutterLogo())
+                  ],
+                ),
               ],
-            )),
-          ),
-          const SliverToBoxAdapter(
-            child: SizedBox(
-              height: 20,
-              child: Center(
-                child: Text('Scroll to see the SliverAppBar in effect.'),
-              ),
             ),
-          ),
-          SliverList(
-            delegate: SliverChildBuilderDelegate(
-              (BuildContext context, int index) {
-                return const ListTile(
-                  title: Text("data"),
-                );
-              },
-              childCount: 20,
-            ),
-          ),
-        ],
-      ),
-      bottomNavigationBar: BottomAppBar(
-        child: Padding(
-          padding: const EdgeInsets.all(8),
-          child: OverflowBar(
-            overflowAlignment: OverflowBarAlignment.center,
-            children: <Widget>[
-              Row(
-                mainAxisSize: MainAxisSize.min,
-                children: <Widget>[
-                  const Text("data"),
-                  const Spacer(),
-                  ElevatedButton(onPressed: () {}, child: const FlutterLogo())
-                ],
-              ),
-            ],
           ),
         ),
       ),
